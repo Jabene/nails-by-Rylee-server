cbd495054d3f34b4c0a9970a6f8532bc74b1616d
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-24 01:27:24 -0700

    Remove static header

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const server = require('./api/server')
 
 server.set('port', process.env.PORT || 8080)
 
-server.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*')
-  next()
-})
+// server.use((request, response, next) => {
+//   response.header('Access-Control-Allow-Origin', '*')
+//   next()
+// })
 
 server.listen(server.get('port'), () => console.log(`Server running on ${server.get('port')}`))
