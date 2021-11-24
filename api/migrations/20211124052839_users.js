052b69831e1361811ba43e3783f14905b1af1c27
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-23 22:37:02 -0700

    Fix syntax error

--- api/migrations/20211124052839_users.js
+++ api/migrations/20211124052839_users.js
@@ -1,13 +1,13 @@
 exports.up = async knex => {
-  await knex.schema.alterTable
-    .table('users', table => {
+  await knex.schema
+    .alterTable('users', table => {
       table.string('email').alter().unique().notNullable()
     })
 };
 
 exports.down = async knex => {
-  await knex.schema.alterTable
-    .table('users', table => {
+  await knex.schema
+    .alterTable('users', table => {
       table.string('email').alter().notNullable()
     })
 };
